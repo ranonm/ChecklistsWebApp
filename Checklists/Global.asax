2665c3d1f2dde294448999f8473555afd8ad64ef
Ranon Martin <rm1905@nova.edu>
2017-04-23 08:53:33 -0400

    Add project files.

---
+++ Checklists/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Checklists.MvcApplication" Language="C#" %>
---
+++ Checklists/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Checklists.Startup))]
+namespace Checklists
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
