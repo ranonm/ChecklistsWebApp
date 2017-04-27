15df5f2a5c0f8fe36f40be0d221e67e046a4c189
Ranon Martin <rm1905@nova.edu>
2017-04-27 06:11:40 -0400

    Add a new todo item to a checklist

---
+++ Checklists/Controllers/TodoItemsController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Checklists.Models;
+using Checklists.ViewModels;
+
+namespace Checklists.Controllers
+{
+    public class TodoItemsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TodoItemsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [Route("checklists/{id}/items/new")]
+        public ActionResult New(int? id)
+        {
+            if (id == null)
+                return HttpNotFound();
+
+            var isChecklistExists = _context.Checklists.Any(c => c.Id == id);
+
+            if (!isChecklistExists)
+                return HttpNotFound();
+
+            var todoItem = new TodoItem
+            {
+                ChecklistId = (int)id
+            };
+
+            return View(todoItem);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Save(TodoItem item)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("New", item);
+            }
+
+            _context.TodoItems.Add(item);
+            _context.SaveChanges();
+
+            return RedirectToAction("items", "Checklists", new { Id = item.ChecklistId });
+        }
+    }
+}
---
+++ Checklists/ViewModels/NewTodoItemViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Checklists.Models;
+
+namespace Checklists.ViewModels
+{
+    public class NewTodoItemViewModel
+    {
+        public string Title { get; set; }
+        public TodoItem TodoItem { get; set; }
+    }
+}
