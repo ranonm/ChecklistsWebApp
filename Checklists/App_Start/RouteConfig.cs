f1172115b633a4f9c1fdc52e8e9d101478b92a55
Ranon Martin <rm1905@nova.edu>
2017-04-29 06:08:18 -0400

    Define the checklist listing page as the home page

--- Checklists/App_Start/RouteConfig.cs
+++ Checklists/App_Start/RouteConfig.cs
@@ -18,7 +18,7 @@ namespace Checklists
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Checklists", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
