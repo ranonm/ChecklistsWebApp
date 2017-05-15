7a4225a34f87cc266781749adc75b7a2ef5fa4b1
Ranon Martin <rm1905@nova.edu>
2017-05-15 06:29:03 -0400

    Prep for release

--- Checklists/Controllers/AccountController.cs
+++ Checklists/Controllers/AccountController.cs
@@ -170,7 +170,7 @@ namespace Checklists.Controllers
                     // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
 
-                    return RedirectToAction("Index", "Home");
+                    return RedirectToAction("Index", "Checklists");
                 }
                 AddErrors(result);
             }
